6ac9859b5ea5b557386b6b01967ed8cde449ab66
RetroAk <60184725+RetroAk@users.noreply.github.com>
2021-02-22 20:16:10 +0530

    Update sll.c

--- sll.c
+++ sll.c
@@ -3,12 +3,12 @@
 
 struct node 
 {
-    int num;                        //Data of the node
-    struct node *nextptr;           //Address of the next node
+    int num;                       
+    struct node *nextptr;    
 }*stnode;
 
-void createNodeList(int n); // function to create the list
-void displayList();         // function to display the list
+void createNodeList(int n);
+void displayList();     
 
 int main()
 {
@@ -29,20 +29,20 @@ void createNodeList(int n)
     int num, i;
     stnode = (struct node *)malloc(sizeof(struct node));
 
-    if(stnode == NULL) //check whether the fnnode is NULL and if so no memory allocation
+    if(stnode == NULL)
     {
         printf(" Memory can not be allocated.");
     }
     else
     {
-// reads data for the node through keyboard
+
 
         printf(" Input data for node 1 : ");
         scanf("%d", &num);
         stnode->num = num;      
-        stnode->nextptr = NULL; // links the address field to NULL
+        stnode->nextptr = NULL; 
         tmp = stnode;
-// Creating n nodes and adding to linked list
+
         for(i=2; i<=n; i++)
         {
             fnNode = (struct node *)malloc(sizeof(struct node));
@@ -56,10 +56,10 @@ void createNodeList(int n)
                 printf(" Input data for node %d : ", i);
                 scanf(" %d", &num);
  
-                fnNode->num = num;      // links the num field of fnNode with num
-                fnNode->nextptr = NULL; // links the address field of fnNode with NULL
+                fnNode->num = num;      
+                fnNode->nextptr = NULL; 
  
-                tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
+                tmp->nextptr = fnNode; 
                 tmp = tmp->nextptr; 
             }
         }
@@ -77,8 +77,8 @@ void displayList()
         tmp = stnode;
         while(tmp != NULL)
         {
-            printf(" Data = %d\n", tmp->num);       // prints the data of current node
-            tmp = tmp->nextptr;                     // advances the position of current node
+            printf(" Data = %d\n", tmp->num);       
+            tmp = tmp->nextptr;       
         }
     }
 } 
